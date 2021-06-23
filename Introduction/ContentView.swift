babbd10faca495b591236c2c521580786670ea00
isaac <baa44422@gmail.com>
2021-06-23 10:29:56 +0800

    V 1-1 Added some pictures and animations, and also added a quiz (mini)

--- Introduction/ContentView.swift
+++ Introduction/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rotation: Angle = .zero
+    @State private var correctAnswer = false
+    @State private var isPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                Image("flowers")
+                    .resizable()
+                    .frame(width: 400, height: 300, alignment: .top)
+                    .shadow(radius: 5)
+                    .rotationEffect(rotation)
+                    .onAppear {
+                        withAnimation(.linear(duration: 0.3).repeatForever(autoreverses: false)) {
+                            rotation = Angle(degrees: 360)
+                            
+                        }
+                    }
+                Spacer()
+                    .frame(height: 50)
+                Text("Guess what this picture is?")
+                
+                HStack {
+                    Button("Flower") {
+                        isPresented = true
+                        correctAnswer = true
+                    }
+                    .padding()
+                    Button("Weird Thing"){
+                        correctAnswer = false
+                        isPresented  = true
+                    }
+                    .padding()
+                    }
+                
+                
+                }
+            }
+        alert(isPresented: $isPresented) {
+            
+            Alert(title: Text(correctAnswer ? "Correct!!" : "HOW???"), message: Text(correctAnswer ? "Wow, you did it!! It wasn't so difficult after all!" : "😾 IT WAS SO EASY!! stupid baka"), dismissButton: Alert.Button.default(Text(correctAnswer ? "Tks!" : "You don't have to remind me for god sake!"))
+                  
+            )}
+        }
+    
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
