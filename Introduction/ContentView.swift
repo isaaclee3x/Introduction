d04caccb99ba8034b2a78e857a573fdb9b1c5531
isaac <baa44422@gmail.com>
2021-06-23 10:54:06 +0800

    V 1 - 2 Added a tab view to this and proceeded to make this page a sort of "recognition" page

--- Introduction/ContentView.swift
+++ Introduction/ContentView.swift
@@ -14,42 +14,46 @@ struct ContentView: View {
     @State private var isPresented = false
     
     var body: some View {
-        ZStack {
-            VStack {
-                Image("flowers")
-                    .resizable()
-                    .frame(width: 400, height: 300, alignment: .top)
-                    .shadow(radius: 5)
-                    .rotationEffect(rotation)
-                    .onAppear {
-                        withAnimation(.linear(duration: 0.3).repeatForever(autoreverses: false)) {
-                            rotation = Angle(degrees: 360)
-                            
+        TabView {
+            ZStack {
+                VStack {
+                    Image("flowers")
+                        .resizable()
+                        .frame(width: 400, height: 300, alignment: .top)
+                        .shadow(radius: 5)
+                        .rotationEffect(rotation)
+                        .onAppear {
+                            withAnimation(.linear(duration: 0.3).repeatForever(autoreverses: false)) {
+                                rotation = Angle(degrees: 360)
+                                
+                            }
                         }
+                    Spacer()
+                        .frame(height: 50)
+                    Text("Guess what this picture is?")
+                    
+                    HStack {
+                        Button("Flower") {
+                            isPresented = true
+                            correctAnswer = true
+                        }
+                        .padding()
+                        Button("Weird Thing"){
+                            correctAnswer = false
+                            isPresented  = true
+                        }
+                        .padding()
                     }
-                Spacer()
-                    .frame(height: 50)
-                Text("Guess what this picture is?")
-                
-                HStack {
-                    Button("Flower") {
-                        isPresented = true
-                        correctAnswer = true
-                    }
-                    .padding()
-                    Button("Weird Thing"){
-                        correctAnswer = false
-                        isPresented  = true
-                    }
-                    .padding()
-                    }
-                
-                
                 }
             }
+            .tabItem {
+                Image(systemName: "person.circle")
+                Text("Recognition")
+            }
         }
-
-
+    }
+    
+    
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
