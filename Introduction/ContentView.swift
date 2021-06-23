8538a560a9d376aef3d4881f37bc76bde883971f
isaac <baa44422@gmail.com>
2021-06-23 10:27:19 +0800

    Initial Commit

---
+++ Introduction/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Introduction
+//
+//  Created by Isaac Lee Jing Zhi on 23/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Introduction/IntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionApp.swift
+//  Introduction
+//
+//  Created by Isaac Lee Jing Zhi on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct IntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
